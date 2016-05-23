56cb908c05be03e06fc9e26f62359d0c157348d3
chrkolst <christopher.kolstad@finn.no>
2016-05-23 12:21:19 +0200

    Added task for getting words from a new line seperated list

--- src/WordReader.elm
+++ src/WordReader.elm
@@ -2,21 +2,26 @@ module WordReader exposing (..)
 
 import Html
 import Html exposing (..)
+import Http exposing (getString)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
 import Html.App as Html
-import Array exposing (Array)
+import Array exposing (Array, fromList)
 import Random exposing (generate, Generator)
 import Random.Array exposing (sample)
 import Time exposing (millisecond, Time)
+import Task exposing (Task)
+import Json.Decode exposing (list, string)
+import String as String
 
 
 type alias Model =
     { words : Array String
     , interval : Float
     , playing : Bool
-    , wordList : Int
+    , wordCount : Int
     , currentWord : Maybe String
+    , lastMessage : String
     }
 
 
@@ -24,22 +29,23 @@ type Msg
     = NoOp
     | UpdateInterval Float
     | Tick Float
-    | StartWords Int
+    | StartWords
     | SelectedWord (Maybe String)
+    | FetchSucceed (Array String)
+    | FetchFail Http.Error
     | StopWords
 
 
 initalModel : Model
 initalModel =
-    { words = Array.fromList [ "Hello", "Goodbye" ], interval = 0, playing = False, wordList = 100, currentWord = Nothing }
+    { words = Array.fromList [ "Hello", "Goodbye" ], interval = 500, playing = False, wordCount = 0, currentWord = Nothing, lastMessage = "" }
 
 
 wordButtons : Model -> Html Msg
 wordButtons model =
     div [ style [ ( "float", "left" ) ] ]
-        [ button [ class "b", onClick (StartWords 100) ] [ text "100 ord" ]
-        , button [ class "b", onClick (StartWords 200) ] [ text "200 ord" ]
-        , button [ class "b", onClick (StartWords 300) ] [ text "300 ord" ]
+        [ button [ class "b", onClick StartWords ] [ text "Start!" ]
+        , button [ class "b", onClick StopWords ] [ text "Ta en pause!" ]
         ]
 
 
@@ -76,7 +82,16 @@ view model =
         , div [ class "center" ]
             [ wordBox model
             ]
+        , div [ class "center" ]
+            [ text model.lastMessage
+            , text (toString model.wordCount)
+            ]
         ]
+
+
+fetchWords : Task Http.Error String
+fetchWords =
+    Http.getString "http://chriswk.github.io/wordreader/data/ord1000.txt"
 
 
 selectNewWord model =
@@ -85,7 +100,10 @@ selectNewWord model =
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    Time.every (model.interval * millisecond) Tick
+    if model.playing then
+        Time.every (model.interval * millisecond) Tick
+    else
+        Sub.none
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -95,9 +113,9 @@ update msg model =
             ( model, Cmd.none )
 
         UpdateInterval time ->
-            ( { model | interval = time }, Cmd.none )
+            ( { model | interval = time, playing = True }, Cmd.none )
 
-        StartWords wordList ->
+        StartWords ->
             ( { model | playing = True }, Random.generate SelectedWord (selectNewWord model) )
 
         SelectedWord word ->
@@ -109,10 +127,34 @@ update msg model =
         Tick time ->
             ( model, Random.generate SelectedWord (selectNewWord model) )
 
+        FetchSucceed wordList ->
+            ( { model | words = wordList, wordCount = Array.length wordList }, Cmd.none )
+
+        FetchFail e ->
+            ( { model | lastMessage = toString e }, Cmd.none )
+
+
+getWords : Cmd Msg
+getWords =
+    let
+        url =
+            "http://chriswk.github.io/wordreader/data/ord1000.txt"
+
+        wordList =
+            Http.getString url
+
+        splitOnNewLine =
+            Task.map (\s -> String.split "\n" s) wordList
+
+        toArray =
+            Task.map fromList splitOnNewLine
+    in
+        Task.perform FetchFail FetchSucceed toArray
+
 
 init : ( Model, Cmd Msg )
 init =
-    ( initalModel, Cmd.none )
+    ( initalModel, getWords )
 
 
 main =
